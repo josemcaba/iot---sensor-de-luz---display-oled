9a25083bb579449924e7654fd66225ad18775f40
josemcaba <96978309+josemcaba@users.noreply.github.com>
2022-11-06 19:34:36 +0100

    Mostramos texto y valores SIN NEWLINE

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 OLED.init(128, 64)
 basic.forever(function () {
     OLED.clear()
-    OLED.writeStringNewLine("Nivel de luz:")
-    OLED.writeNumNewLine(Environment.ReadLightIntensity(AnalogPin.P2))
+    OLED.writeString("Nivel de luz:")
+    OLED.writeNum(Environment.ReadLightIntensity(AnalogPin.P2))
 })
