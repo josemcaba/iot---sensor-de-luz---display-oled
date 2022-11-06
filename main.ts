fe243e46ea83b302f1a6ad75c99a2ba81faaec01
josemcaba <96978309+josemcaba@users.noreply.github.com>
2022-11-06 19:29:28 +0100

    Primera versión

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+OLED.init(128, 64)
 basic.forever(function () {
-	
+    OLED.clear()
+    OLED.writeStringNewLine("Nivel de luz:")
+    OLED.writeNumNewLine(Environment.ReadLightIntensity(AnalogPin.P2))
 })
